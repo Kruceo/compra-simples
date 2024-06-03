9e527be98ecb51b5dc325a411e8faf070e46ab3e
kruceo <kruceo.com@gmail.com>
2024-06-03 15:45:18 -0300

    fix: improve login page

--- frontend/src/components/pages/Login/LoginScreen.tsx
+++ frontend/src/components/pages/Login/LoginScreen.tsx
@@ -7,11 +7,12 @@ import Cookies from "js-cookie";
 
 export default function LoginScreen() {
     const navigate = useNavigate()
-
+    const [loading,setLoading] = useState(false);
     const [error, setError] = useState("")
     const [message, setMessage] = useState("")
     const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+       
         const data = new FormData(e.currentTarget)
         const user = data.get("user")
         const password = data.get("password")
@@ -19,23 +20,33 @@ export default function LoginScreen() {
         if (!user) return setError('user')
         if (!password) return setError('password')
 
-        const response = await backend.auth.login(user.toString(), password.toString())
-
-        if (response.error) return setMessage(response.message)
-        if (response.token) {
-            Cookies.set('token', response.token, { domain: api_address, expires: new Date(response.expiresIn) })
-            navigate("/")
+        try {
+            setMessage("")
+            setError("")    
+            setLoading(true)
+            const response = await backend.auth.login(user.toString(), password.toString())
+            setLoading(false)
+            if (response.error) return setMessage(response.message)
+            if (response.token) {
+                Cookies.set('token', response.token, { domain: api_address, expires: new Date(response.expiresIn) })
+                navigate("/")
+            }
+        } catch (error) {
+            setLoading(false)
+            setMessage("Não foi possível se conectar ao servidor.")
         }
+        
+
     }
 
     return <div className="flex justify-center items-center h-screen w-screen bg-no-repeat bg-cover" style={{ backgroundImage: "url(https://www.bing.com/th?id=OBTQ.BT5E5226697BC9627BA6BA4740AED82E5A51E949E7F70321A9B83A0819BAFEC98A&rs=2&c=1)" }}>
-        <form onSubmit={submitHandler} className="flex flex-col w-80 bg-subpanel p-4 border-borders border shadow-xl bg-cover bg-no-repeat">
+        <form onSubmit={submitHandler} className={"flex flex-col w-80 bg-subpanel p-4 border-borders border shadow-xl bg-cover bg-no-repeat " + (loading?"cursor-wait brightness-50 relative overflow-hidden after:content-[''] after:absolute after:left-0 after:top-0 after:block after:w-full after:h-full after:z-50 after:animate-skeleton-fade":"")}>
             <h2>Login</h2>
             <RequiredLabel>Usuário</RequiredLabel>
             <FormInput placeholder="Insira seu usuário" name="user" type="text" errored={error == "user"} autoFocus />
             <RequiredLabel>Senha</RequiredLabel>
             <FormInput placeholder="Insira sua senha" name="password" type="password" errored={error == "password"} />
-            <FormInput type="submit" value={"Entrar"} />
+            <FormInput type="submit" value={loading?"Carregando":"Entrar"} disabled={loading} />
             <p className="my-4 h-5 text-red-500 text-center">{message}</p>
         </form>
     </div>
